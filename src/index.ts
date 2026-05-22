a2d11ccec4e01ea35b95fbb871c8fd817376c569
pranjal1606 <pranjalahir69@gmail.com>
2026-05-22 12:44:03 +0530

    index file and env ex

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import "reflect-metadata";
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("To-Do Management API is running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
