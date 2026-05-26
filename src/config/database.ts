e54beecd10f282ac8cc3e000763f4f4d085f4b55
pranjal1606 <pranjalahir69@gmail.com>
2026-05-26 11:20:22 +0530

    migration and postman added

--- src/config/database.ts
+++ src/config/database.ts
@@ -10,7 +10,7 @@ export const db = new DataSource({
   username: process.env.DB_USER as string,
   password: process.env.DB_PASSWORD as string,
   database: process.env.DB_NAME as string,
-  synchronize: true, // Auto-create tables (turn off in production)
+  synchronize: false, // Auto-create tables (turn off in production)
   // Logging means: showing database activity in the terminal/console. TypeORM can print:, SQL queries, errors, warnings, schema changes, migrations
   logging: false,
   // entities: which classes are TypeORM entities (tables) in your project. TypeORM needs this to create tables.
@@ -18,5 +18,5 @@ export const db = new DataSource({
   // subscribers: classes that listen to TypeORM events (like beforeInsert, afterLoad). You can leave this empty if you don't need event listeners.
   subscribers: [],
   // migrations: SQL scripts that create/update your database schema (tables, columns, indexes). They are run manually.
-  migrations: [],
+  migrations: ["src/migrations/*.ts"],
 });
