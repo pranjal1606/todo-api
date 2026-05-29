e2dd50542611e6707a578750a2895634ffeb6005
pranjal1606 <pranjalahir69@gmail.com>
2026-05-29 18:02:43 +0530

    register rate limit edited

--- src/auth/middlewares/rateLimiter.middleware.ts
+++ src/auth/middlewares/rateLimiter.middleware.ts
@@ -2,12 +2,12 @@ import rateLimit from "express-rate-limit";
 
 // Limit repeated login and verify requests to prevent brute-force attacks
 export const authRateLimiter = rateLimit({
-  windowMs: 10 * 60 * 1000, // 10 minutes
+  windowMs: 24 * 60 * 60 * 1000, // 24 hours
   max: 3, // Limit each client to 3 requests per `windowMs`
   message: {
     status: "fail",
     message:
-      "Too many login or verification attempts. Please try again after 15 minutes.",
+      "Too many login or verification attempts. Please try again after 24 hours.",
   },
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
